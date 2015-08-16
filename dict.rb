ba2677301b3a10d478a37c61dd3b5a190a0305cd
Spitsman <sanspits@yandex.ru>
2015-08-16 23:41:38 +0400

    add 'connect'

--- dict.rb
+++ dict.rb
@@ -3,16 +3,23 @@ require "httparty"
 class Dictionary
 	attr_accessor :api_key
 	def initialize(key)
-		api_key = key
+		@api_key = key
+	end
+
+	def connect(path, options = [])
+		url = "https://dictionary.yandex.net/api/v1/dicservice.json/"
+		url << path
+		url << "?key=#{@api_key}"
+		url << "&lang=#{options[0]}&text=#{options[1]}" unless options.empty?
+		response = HTTParty.get(url)
 	end
 
 	def get_langs
-		url = "https://dictionary.yandex.net/api/v1/dicservice.json/getLangs?key=#{api_key}"
-		response = HTTParty.get(url)
+		connect('getLangs')
 	end
 
 	def lookup(lang, text)
-		url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=#{api_key}&lang=#{lang}&text=#{text}"
-		response = HTTParty.get(url)
+		connect('lookup', [lang, text])
 	end
 end
+
