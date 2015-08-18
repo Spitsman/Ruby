cabb4d0f26871614d73d0e95841c3688477957fa
Spitsman <sanspits@yandex.ru>
2015-08-18 15:37:25 +0400

    add check errors

--- dict.rb
+++ dict.rb
@@ -7,11 +7,12 @@ class Dictionary
 	end
 
 	def connect(path, options = [])
-		url = "https://dictionary.yandex.net/api/v1/dicservice.json/"
+		url = "https://dictionary.yandex.net/api/v1/dicservice/"
 		url << path
 		url << "?key=#{@api_key}"
 		url << "&lang=#{options[0]}&text=#{options[1]}" unless options.empty?
 		response = HTTParty.get(url)
+		check_errors(response) unless response.code == 200
 	end
 
 	def get_langs
@@ -21,5 +22,19 @@ class Dictionary
 	def lookup(lang, text)
 		connect('lookup', [lang, text])
 	end
+
+	def check_errors(response)
+		case response.code 
+		when 401 then puts 'Invalid API key'
+		when 402 then puts 'API key blocked'
+		when 403 then puts 'Daily request limit exceeded'
+		when 404 then puts 'Daily char limit exceeded'
+    	when 413 then puts 'Text too long'
+    	when 501 then puts 'Can\'t translate text in that direction'
+		end
+	end
 end
 
+dict = Dictionary.new('dict.1.1.20150814T100205Z.a0d27651d642b1d1.93ce6ba9cd891aada1fb98d47b6cd89c15a32f2e')
+dict.get_langs
+dict.lookup('en-ru', 'cat')
