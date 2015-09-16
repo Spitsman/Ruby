a4f8ff4c987bf82e5c69e20dc9bef6f61320508b
Spitsman <sanspits@yandex.ru>
2015-09-16 22:03:17 +0400

    commit

--- spec/lib/parser_spec.rb
+++ spec/lib/parser_spec.rb
@@ -4,9 +4,9 @@ describe Parser do
 	
 	it 'parse' do
 		key = 'dict.1.1.20150814T100205Z.a0d27651d642b1d1.93ce6ba9cd891aada1fb98d47b6cd89c15a32f2e'
-		client = DictionaryAPI::Client.new(key)
+		client = Client.new(key)
 		response = JSON.parse(client.execute('lookup', ['en-ru', 'time']).body)
-		parser = DictionaryAPI::Parser.new
+		parser = Parser.new
 		parser.parse(response).size.should eq 3
 	end
 	
