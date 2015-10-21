6329a651e8ad569c5fd3edf7554905c7e3b68c37
Spitsman <sanspits@yandex.ru>
2015-10-21 22:52:08 +0300

    add attributes method to articles

--- lib/dictionary_api/article.rb
+++ lib/dictionary_api/article.rb
@@ -1,7 +1,13 @@
 module DictionaryAPI
 
   class Article
+
     attr_accessor :text, :position, :transcription, :translation
+
+    def attributes
+  	  {text: text, position: position, transcription: transcription}
+	  end
+
   end
 
 end
--- lib/dictionary_api/translation_article.rb
+++ lib/dictionary_api/translation_article.rb
@@ -1,7 +1,13 @@
 module DictionaryAPI
 
   class TranslationArticle
+  	
     attr_accessor :text, :position, :gender, :animated, :synonym, :meaning, :example, :aspect
+  	
+  	def attributes
+	  	{text: text, position: position, gender: gender, animated: animated, synonym: synonym, meaning: meaning, example: example, aspect: aspect}
+	  end
+
   end
 
 end
